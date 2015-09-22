9bdf2dc17de2bd8c2cd282fe5643f60efff08713
Brian Momongan <bmomonganfpt@gmail.com>
2015-09-22 14:45:40 +0800

    first commit

---
+++ src/main/java/org/spring/prac/SpringBootPracApplication.java
@@ -0,0 +1,12 @@
+package org.spring.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPracApplication.class, args);
+    }
+}
